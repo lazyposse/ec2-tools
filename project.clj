021c7aa5cf83f41227ec684af37fe6faa0ad235b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-10-02 18:53:56 +0200

    Make this namespace a project.

--- project.clj
+++ project.clj
@@ -1,6 +1,14 @@
 (defproject ec2-tools "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Small ec2 lib to admin ec2 accounts."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :profiles {:dev
+             {:dependencies
+              [[midje "1.4.0"]
+               [table "0.3.1" :exclusions [com.datomic/datomic]]]}}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-http                              "0.4.0"]
+                 [clj-time                              "0.4.4"]
+                 [org.clojure/data.codec                "0.1.0"]
+                 [digest                                "1.3.0"]])
