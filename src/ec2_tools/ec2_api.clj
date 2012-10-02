989c56b3c1cfe33c38274b2d3dfb9f093f389d74
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-10-02 20:29:35 +0200

    Give an example on howto parse the result xml.

--- src/ec2_tools/ec2_api.clj
+++ src/ec2_tools/ec2_api.clj
@@ -1,6 +1,7 @@
 (ns ec2-tools.ec2-api
   "The main entry to query your amazon account"
-  (:require [ec2-tools.ec2-query :as q]))
+  (:require [ec2-tools.ec2-query :as q]
+            [clojure.xml         :as x]))
 
 (defn ls-region
   [region]
@@ -12,5 +13,5 @@
 
 (comment
   (def regions (ls-regions))
-  (-> regions :body)
+  (-> regions :body (.getBytes) (java.io.ByteArrayInputStream.) (x/parse))
   (def region-us-east-1 (ls-region "us-east-1")))
