32563ce1f20433449d078c7ad6c4cea0d2fc49e5
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-10-04 08:20:11 +0200

    * DONE Fix the setup inside the ec2-tools/ec2-query namespace

--- src/ec2_tools/ec2_query.clj
+++ src/ec2_tools/ec2_query.clj
@@ -1,26 +1,16 @@
 (ns ec2-tools.ec2-query
+  "A namespace to permit the construction of amazon query"
   (:require [clj-http.client            :as c]
-            [ec2-tools.ec2-authenticate :as a]
-            [ec2-tools.ec2-hook :as h]))
-
-;; ################### SETUP
-
-(def ^{:private true
-       :doc "The ec2 host"}
-  ec2-host "ec2.amazonaws.com")
-
-(def ^{:private true
-       :doc "The main access to the ec2 web services."}
-  url (str "https://" ec2-host))
+            [ec2-tools.ec2-authenticate :as a]))
 
 (defn amazon-query
   "Querying amazon's account"
-  [method path & [opts]]
+  [method params & [opts]]
   (c/request
    (merge {:method     method
-           :url        (str url \? (a/compute-url-parameters path))
+           :url        (a/compute-url params)
            :accept     :xml
            :as         :xml}
           opts)))
 
-(h/add-hook #'amazon-query #'log-hook)
+(rh/add-hook #'amazon-query #'h/log-hook)
